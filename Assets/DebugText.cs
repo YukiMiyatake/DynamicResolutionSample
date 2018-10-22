3e231f17a443e24553ff8db25237d7696237fb0a
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2018-10-22 16:40:47 +0900

    とりあえず動く状態

--- Assets/DebugText.cs
+++ Assets/DebugText.cs
@@ -7,24 +7,27 @@ using UnityEngine.UI;
 public class DebugText : MonoBehaviour
 {
     private Text text_;
+    public FrameTiming ft;
 
     // Start is called before the first frame update
     void Start()
     {
         text_ = GetComponent<Text>();
+ //       frameTimings = new FrameTiming[2];
 
     }
 
 // Update is called once per frame
 void Update()
     {
-        var frameTimings = new FrameTiming[1];
+/*
+        var frameTimings = new FrameTiming[2];
         FrameTimingManager.CaptureFrameTimings();
-        uint numReturened = FrameTimingManager.GetLatestTimings(1, frameTimings);
-        var ft = frameTimings[0];
-
+        uint numReturened = FrameTimingManager.GetLatestTimings(2, frameTimings);
+        var ft = frameTimings[1];
+*/
         text_.text = string.Format(  "Screen  Width:{0,-4} Height:{1,-4}\n" 
-                                   + "cpuFrameTime  {2}\n"
+                                   + "cpuFrameTime  {2:F8}\n"
                                    + "gpuFrameTime  {3}\n"
                                    + "cpuTiminePresentCalled  {4}\n"
                                    + "cpuTimeFrameComplete  {5}\n"
@@ -33,7 +36,8 @@ void Update()
                                    + "syncInterval  {8}\n"
                                    , Screen.width, Screen.height
                                    , ft.cpuFrameTime, ft.gpuFrameTime, ft.cpuTimePresentCalled, ft.cpuTimeFrameComplete
-                                   , ft.heightScale, ft.widthScale, ft.syncInterval);
+                                   , ft.heightScale, ft.widthScale, ft.syncInterval
+                                   );
 
 
     }
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    public DebugText _text;
+    public ScreenScale _screenScale;
+    public UnitychanScale _unitychanScale;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +17,17 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+
         var frameTimings = new FrameTiming[1];
         FrameTimingManager.CaptureFrameTimings();
         uint numReturened = FrameTimingManager.GetLatestTimings(1, frameTimings);
         var ft = frameTimings[0];
 
+        if(numReturened > 0){
+            _text.ft = ft;
+        }
 
-        Debug.Log( "CPU" + frameTimings[0].cpuFrameTime);
-        */
+        //    Debug.Log( "CPU" + frameTimings[0].cpuFrameTime);
+
     }
 }
